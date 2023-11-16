52812b7341b7509fa20d15218bcfd17e462686ca
Joseph Samuel Paez Cobos <135568580+2236571@users.noreply.github.com>
2023-11-15 20:09:42 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template
+import requests
+from dotenv import load_dotenv,dotenv_values
+
+app = Flask(__name__)
+
+config = dotenv_values ('.env')
+
+def get_weather_data(city):
+    API_KEY = config['API_KEY']
+    url=f'https://api.openweathermap.org/data/2.5/weather?q={ city }&appid={API_KEY}&units=metric&lang=es'
+    r = requests.get(url).json()
+    return r 
+
+@app.route('/prueba')
+def prueba():
+    clima = get_weather_data('Cuenca')
+    temperatura = str(clima['main']['temp'])
+    descripcion= str(clima['weather'][0]['description'])
+    icono= str(clima['weather'][0]['icon'])
+
+    r_json= {
+            'ciudad':'Cuenca',
+            'temperatura': temperatura,
+             'descripcion':descripcion,
+             'icono':icono
+             }  
+    return render_template('weather.html',clima=r_json)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/clima')
+def clima():
+    return 'obtener todo la informacion del clima'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
